ef9026c0a7abe4b943d8c71e5866e8300290da1c
anboch <a.g.bochka@gmail.com>
2022-11-06 07:26:11 +0600

    feat: add MaterialPreviewDialog

---
+++ src/components/MaterialPreviewDialog/MaterialPreviewDialog.tsx
@@ -0,0 +1,29 @@
+import { Dispatch, SetStateAction } from 'react';
+import { Dialog } from '@mui/material';
+import { MaterialItem } from '../MaterialItem';
+import { IMaterial } from '../../api/api.interface';
+import { MaterialItemInfo, MaterialItemTitles } from '../MaterialItem/MaterialItem';
+
+interface IMaterialPreviewProps {
+  material: IMaterial | null,
+  setMaterial: Dispatch<SetStateAction<IMaterial | null>>
+}
+
+export const MaterialPreviewDialog = ({ material, setMaterial }: IMaterialPreviewProps): JSX.Element => {
+  const handleCloseDialog = () => { setMaterial(null); };
+
+  return (
+    <Dialog
+      fullWidth
+      scroll={'body'}
+      open={!!material}
+      onClose={handleCloseDialog}>
+      {material &&
+        <div style={{ padding: '10px' }}>
+          <MaterialItemTitles titles={material.titles} />
+          <MaterialItemInfo material={material} />
+        </div>}
+
+    </Dialog>
+  )
+}
