c585e237144c262c7fc671605758bc896814fa3f
anboch <a.g.bochka@gmail.com>
2022-11-19 21:46:17 +0600

    feat: add dark mode for maps

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,5 +1,6 @@
 import MapGL, { NavigationControl } from '@urbica/react-map-gl';
-import { ReactNode, useState } from 'react';
+import { ReactNode, useContext, useState } from 'react';
+import { ThemeContext, themeModeTypes } from '../../context/ThemeContex/ThemeContext';
 
 interface IViewportSettings {
   latitude: number;
@@ -18,15 +19,21 @@ const mapCommonSettings = {
   },
 } as const;
 
+// todo add handle error if REACT_APP_MAPBOX_ACCESS_TOKEN is undefined
 export const Map = ({ children }: { children: ReactNode }): JSX.Element => {
+  const { themeMode } = useContext(ThemeContext);
   const [viewport, setViewport] = useState<IViewportSettings>(mapCommonSettings.defaultViewport);
 
   return (
     <>
       <MapGL
         scrollZoom={false}
-        style={{ width: '100%', height: '400px' }}
-        mapStyle="mapbox://styles/anboch/cla096w1a00ab15s2lllm4o89"
+        style={{ width: '100%', height: '60vh' }}
+        mapStyle={
+          themeMode === themeModeTypes.LIGHT
+            ? 'mapbox://styles/anboch/cla096w1a00ab15s2lllm4o89'
+            : 'mapbox://styles/anboch/clamp54r4000714l86046tm8o'
+        }
         accessToken={mapCommonSettings.mapboxPublicAccessToken}
         latitude={viewport.latitude}
         longitude={viewport.longitude}
