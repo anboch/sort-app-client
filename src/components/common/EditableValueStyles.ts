e91c0bbc65dbed76bff1efc8db8c9bbf7bd5661e
anboch <a.g.bochka@gmail.com>
2022-11-22 17:09:48 +0600

    fix: EditableValue styles

--- src/components/common/EditableValueStyles.ts
+++ src/components/common/EditableValueStyles.ts
@@ -15,13 +15,14 @@ export const Value = styled.div`
   display: flex;
   justify-content: center;
   margin-top: -24px;
+  & > * {
+    text-align: center;
+    overflow-wrap: anywhere;
+    max-width: 200px;
+  }
   @media (max-width: 500px) {
     & input {
       max-width: 100px;
     }
-  }
-  & * {
-    text-align: center;
-    overflow-wrap: anywhere;
   }
 `;
