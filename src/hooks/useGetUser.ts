ea234efbfea42bd5e9c88763a7f4d57440c33b5d
anboch <a.g.bochka@gmail.com>
2022-12-11 17:20:22 +0600

    fix: change user query retry to 1

--- src/hooks/useGetUser.ts
+++ src/hooks/useGetUser.ts
@@ -8,6 +8,7 @@ export const useGetUser = (): UseQueryResult<IUser> => {
   return useQuery<IUser>([queryKeys.user], api.fetchUser, {
     notifyOnChangeProps: 'all',
     enabled: !!localStorage.getItem(localStorageKeys.accessToken),
+    retry: 1,
     // ToDO think about staleTime(access_token time !?) and cacheTime(refresh_token time !?)
   });
 };
