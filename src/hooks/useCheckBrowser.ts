c77a9bd7c1b9e1e286eb34a1b3a7419548c589f9
anboch <a.g.bochka@gmail.com>
2023-01-29 14:51:35 +0600

    fix: useCheckBrowser

--- src/hooks/useCheckBrowser.ts
+++ src/hooks/useCheckBrowser.ts
@@ -18,18 +18,18 @@ export const useCheckBrowser = (): ValueOf<typeof browserTypes> => {
   const detectBrowser = (): void => {
     const userAgent = navigator.userAgent;
 
-    if (userAgent.match(/chrome|chromium|crios/i)) {
-      setBrowserType(browserTypes.CHROME);
-    } else if (userAgent.match(/safari/i)) {
-      setBrowserType(browserTypes.SAFARI);
-    } else if (userAgent.match(/yabrowser/i)) {
+    if (userAgent.match(/yabrowser/i)) {
       setBrowserType(browserTypes.YANDEX);
-    } else if (userAgent.match(/samsung/i)) {
-      setBrowserType(browserTypes.SAMSUNG);
     } else if (userAgent.match(/opr\//i)) {
       setBrowserType(browserTypes.OPERA);
-    } else if (userAgent.match(/firefox|fxios/i)) {
+    } else if (userAgent.match(/samsung/i)) {
+      setBrowserType(browserTypes.SAMSUNG);
+    } else if (userAgent.match(/firefox|fxios/i) && !userAgent.match(/Seamonkey/i)) {
       setBrowserType(browserTypes.FIREFOX);
+    } else if (userAgent.match(/safari/i) && !userAgent.match(/Chromium|chrome/i)) {
+      setBrowserType(browserTypes.SAFARI);
+    } else if (userAgent.match(/chrome|crios/i) && !userAgent.match(/Chromium|Edg/i)) {
+      setBrowserType(browserTypes.CHROME);
     }
   };
 
